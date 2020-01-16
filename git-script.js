8905d40d0a9421d857ccd2d23637b8f023d5fa59
ehayman <emily.hayman@datadoghq.com>
2020-01-16 17:14:00 +0100

    testtests

--- git-script.js
+++ git-script.js
@@ -3,6 +3,8 @@ var shell = require("shelljs");
 
 shell.exec("git add -A");
 
+shell.exec("echo 'test'");
+
 shell.exec("git commit -m 'testtests'");
 
 //shell.exec("git push origin master --force");
